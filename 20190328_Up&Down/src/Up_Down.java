825347e4cf9f9114305ebb26496675579babf038
Nayoung <s2018s32@e-mirim.hs.kr>
2019-03-28 19:50:42 +0900

    add UpDown project

---
+++ 20190328_Up&Down/src/Up_Down.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+public class Up_Down { 
+     public static void main(String[] args) { 
+     int random = (int)(Math.random()*100) +1; 
+     System.out.println("1~100 사이의 숫자를 골랐으니 숫자를 맞춰 보시오."); 
+     Scanner scanner = new Scanner(System.in); 
+
+          while(true){ 
+               System.out.print("숫자 입력 >> ");
+               int input = scanner.nextInt(); 
+               if(random != input && random > input){ 
+                    System.out.println("Up!");
+               }
+               else if(random != input && random < input){ 
+                    System.out.println("Down!"); 
+               }
+               else if(random == input){ 
+                    System.out.println("정답입니다!"); 
+                    System.exit(0); 
+               } 
+          } 
+     }
+}
---
+++ 20190328_Up&Down/src/Up_Down_Bot.java
@@ -0,0 +1,9 @@
+
+public class Up_Down_Bot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
