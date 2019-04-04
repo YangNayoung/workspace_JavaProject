4c4d96a0ce98f98461faf78c2ee3c210f48500ed
Nayoung <s2018s32@e-mirim.hs.kr>
2019-04-04 19:56:15 +0900

    add Nsum

---
+++ Nsum/src/Nsum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+public class Nsum {
+
+	public static void main(String[] args) {
+		// 숫자 입력받자 (String으로 입력받자 -> 자릿수를 알기 위해서 -> 0~자릿수-1까지 하나씩 잘라서 더해야함/
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요>> ");
+		String number = sc.nextLine();
+		int sum=0;
+		// 각 자릿수의 수를 더하자
+		int length = number.length();
+		for(int i=0; i<length; i++) {
+			// 0~자릿수-1까지 반복하자
+			// 한글자씩 가져와서 숫자를 바꾸자
+			char n = number.charAt(i);
+			// sum에 더하자
+			sum+=n-'0';
+		}
+		// 더한 값을 출력하자
+		System.out.println(sum);
+	}
+
+}
