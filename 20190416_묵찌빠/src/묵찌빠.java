b56c20ef38c8d2962a41ed59125a79478d965947
Nayoung <s2018s32@e-mirim.hs.kr>
2019-04-16 19:43:15 +0900

    add 묵찌빠

--- 20190416_묵찌빠/src/묵찌빠.java
+++ 20190416_묵찌빠/src/묵찌빠.java
@@ -4,8 +4,12 @@ import java.util.Random;
 public class 묵찌빠 {
 
 	public static void main(String[] args) {
-		String[] pae = {"가위", "바위", "보"};
-		//사용자의 입력을 받는다.(1:가위, 2:바위, 3:보, 0:끝)
+		boolean winplayer = false;
+		boolean winner = false;
+		
+		String[] pae = {"찌", "묵", "빠"};
+		//사용자의 입력을 받는다.(1:찌, 2:묵, 3:빠, 0:끝)
+		System.out.println("사용자의 입력을 말하자(1:찌 2:묵 3:빠 0:종료) ");
 		Scanner sc = new Scanner(System.in);
 		while(true){
 			int p = sc.nextInt();
@@ -14,35 +18,36 @@ public class 묵찌빠 {
 				break;
 			}
 			int c = makeComputer();
-			//사용자와 컴퓨터의 패를 비교하자
 			int result = compare(p, c);
-	/*		if(p == 1) {
-				System.out.println("사용자: 가위");
-			}else if(p == 2) {
-				System.out.println("사용자: 바위");
-			}else if(p == 3) {
-				System.out.println("사용자: 보");
-			}else {
-				System.out.println("잘못 누르셨습니다.");
-			} */
+			
 			System.out.println("사용자: "+pae[p-1]+"\t컴퓨터: "+pae[c-1]);
 			if (result == -1) {
-				System.out.println("졌다ㅠㅠ");
+				winplayer = false;
+				winner = true;
 			} else if(result == 0) {
-				System.out.println("비겼다.");
+				// 처음 비기면 다시
+				if(winner == true) {
+				// 승패가 갈린 다음에 비기며, 게임 끝, 결과를 알려주자
+					if(winplayer == true) {
+						System.out.println("이겼다!!!");
+					} else {
+						System.out.println("졌다ㅠㅠ");
+					}
+				} else {
+					// 처음 비기면 다시.
+				}
 			} else {
-				System.out.println("이겼다!!!");
+				// 이전 결과를 갖고 다시
+				winplayer = true;
+				winner =  true;
 			}
-		//결과를 출력하자
 		}
 	}
 
 	private static int compare(int p, int c) {
-		//if((p == 1 && c == 2) || (p == 2 && c == 3) || (p == 3 && c == 1)) {
-		if((p + 1) % 3 == c) {
+		if((p + 1) % 3 == c % 3) {
 		//-1: 사용자 짐
 			return -1;
-		// } else if ((p == 1 && c == 1) || (p == 2 && c == 2) || (p == 3 && c == 3)) {
 		} else if(p == c) {
 		//0: 비금
 			return 0;
@@ -57,6 +62,4 @@ public class 묵찌빠 {
 		int c=random.nextInt(3-1+1)+1; // 1~3 임의의 수
 		return c;
 	}
-	}
-
 }
