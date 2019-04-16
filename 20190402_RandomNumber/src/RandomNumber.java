0ab005ca90687101964df44177f31b2e2538d3a2
Nayoung <s2018s32@e-mirim.hs.kr>
2019-04-16 19:18:24 +0900

    add 사용자숫자맞추기봇

--- 20190402_RandomNumber/src/RandomNumber.java
+++ 20190402_RandomNumber/src/RandomNumber.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class RandomNumber {
 
@@ -7,26 +7,36 @@ public class RandomNumber {
 		// TODO Auto-generated method stub
 		int start = 0;
 		int end = 99;
+		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		int RandomNumber = random.nextInt(end+1-start)+start;
-		System.out.println(RandomNumber);
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			int input = sc.nextInt();
+		int randomNumber = random.nextInt(end+1-start)+start;
+		
+		System.out.println("1~100까지 숫자 중 하나를 생각하세요.");
+		System.out.println("숫자가 생각한 것보다 크다면 (DOWN)'2'을 작다면 (UP)'1'을 입력하고 맞으면 '0'을 입력하세요.");
+		
+		while(true) {
 			
-			if(input == 2) {
-				//up -> start 교체, guess 추측
-				start = guess + 1;
-			}
-			else if(input == 3) {
-				//down -> end 교체, guess 추측
-				
-			}
-			else if(input == 1) {
+			int guess = random.nextInt(end-start+1)+start;
+			//int guess = (end-start)/2;//최적화 기법
+			System.out.println(guess+"입니까? (0 : 정답, 1 : up, 2 : down)");
+			//input(up/down/점답/) (0 : 정답, 1 : up, 2 : down)
+			int answer = scanner.nextInt();
+			
+			if(answer == 0) {
 				System.out.println("정답입니다.");
 				break;
 			}
-			//== -> "정답입니다.", break
+			else if(answer == 1) {
+				//up이면 guess+1 ~end
+				start = guess + 1;
+			}
+			else if(answer == 2) {
+				//down이면 start ~ guess-1
+				end = guess - 1;
+			}
+
+				
+
 		}
 	}
 
