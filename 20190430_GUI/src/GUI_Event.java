12bcac0d86cf434f3ab76f96eebf13fa8126a64f
Nayoung <s2018s32@e-mirim.hs.kr>
2019-05-28 19:53:04 +0900

    add GUI_Counter Button

--- 20190430_GUI/src/GUI_Event.java
+++ 20190430_GUI/src/GUI_Event.java
@@ -14,13 +14,16 @@ public class GUI_Event {
 		frame.setPreferredSize(new Dimension(400,600));
 		
 		JTextField tfInput = new JTextField(10);
-		JButton btRun = new JButton("->");
+		JButton btRun = new JButton("X 12");
 		JLabel lbOutput = new JLabel();
 		
 		ActionListener al = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String input = tfInput.getText();
-				lbOutput.setText("¾È³ç! "+input+"¾ß");
+				// lbOutput.setText("¾È³ç! "+input+"¾ß"); // ¾È³ç! ¤±¤±¾ß
+				int i = Integer.parseInt(input);
+				String Output = Integer.toString(i*12);
+				lbOutput.setText(Output);
 			}
 		};
 		
