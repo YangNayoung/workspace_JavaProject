06edbf309ff4700f7a4b783b13a53bfe4e833054
Nayoung <s2018s32@e-mirim.hs.kr>
2019-05-28 19:15:02 +0900

    add GUI_Event

---
+++ 20190430_GUI/src/GUI_Event.java
@@ -0,0 +1,39 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUI_Event {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Event");
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(400,600));
+		
+		JTextField tfInput = new JTextField(10);
+		JButton btRun = new JButton("->");
+		JLabel lbOutput = new JLabel();
+		
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String input = tfInput.getText();
+				lbOutput.setText("¾È³ç! "+input+"¾ß");
+			}
+		};
+		
+		btRun.addActionListener(al);
+		
+		panel.add(tfInput);
+		panel.add(btRun);
+		panel.add(lbOutput);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
